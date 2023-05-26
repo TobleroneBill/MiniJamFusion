54a7931115cf58eb86b75c5642ff896e48e92ac9
TobeleroneBill <billseaton2.0@gmail.com>
2023-05-26 10:52:00 +0100

    Base Mechanics

--- Levels/Test.gd
+++ Levels/Test.gd
@@ -1,6 +1,39 @@
 extends Node
 
+var present = true
+
+func TimeTravel():
+	print("timetravel")
+	# switch to past
+	
+	if present:
+		for node in $Present.get_children():
+			node.add_collision_exception_with($Player)
+			node.visible = false
+			
+		for node in $Past.get_children():
+			node.remove_collision_exception_with($Player)
+			node.visible = true
+		present = !present
+	
+	else:
+		for node in $Past.get_children():
+			node.visible = false
+			node.add_collision_exception_with($Player)
+			
+		for node in $Present.get_children():
+			node.remove_collision_exception_with($Player)
+			node.visible = true
+			
+		present = !present
+		
+		
+
+func _ready():
+	TimeTravel()
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
+
+
--- Player/player.gd
+++ Player/player.gd
@@ -22,9 +22,8 @@ func _physics_process(delta):
 		print('globalPos: ' + str(global_position))
 		var actualPos = Vector2(raycast.global_position.x - global_position.x,raycast.global_position.y - global_position.y)
 		print(global_position + actualPos)
-		
-		
-		
-		#apply_impulse(Vector2(actualPos.x*Recoilx,actualPos.y*Recoily),actualPos)
 		apply_impulse(-actualPos * Recoil,actualPos)
 		print('Shoots')
+	
+	if Input.is_action_just_pressed("travel"):
+		get_parent().TimeTravel()
